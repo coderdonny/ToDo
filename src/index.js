027d2635ffc5ecd2d24287805846f4ae15b1a165
Daniel Karpierz <dankarp1999@gmail.com>
2022-09-10 15:46:02 -0400

    Create a simple UI with tailwindcss

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 import './style.css';
+
+console.log('tailwindcss webpack tes hello');
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,28 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./dist/*.html'],
 	theme: {
-		extend: {},
-	},
-	variants: {
-		extend: {},
+		screens: {
+			sm: '480px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
+		extend: {
+			fontFamily: {
+				raj: ["'Public Sans', sans-serif"],
+				public: ["'Rajdhani', sans-serif"],
+			},
+			colors: {
+				darkBlue: '#0E172A',
+				darkWashedBlue: '#1E293B',
+				buttonBlue: '#6466F1',
+				highlightBlue: '#0CA5E9',
+				lightBlue: '#A5B4FB',
+				OOFGrey: '#94A3B8',
+				offWhite: '#E2E8F0',
+			},
+		},
 	},
 	plugins: [],
 };
