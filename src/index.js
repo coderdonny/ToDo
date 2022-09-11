fa2df07f3cf63f4ee79c0643748c5da89edf339e
Daniel Karpierz <dankarp1999@gmail.com>
2022-09-11 13:27:57 -0400

    Add 'Create Task' module for task creation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './style.css';
+import Task from './create-task';
 
-console.log('tailwindcss webpack tes hello');
+console.log('tailwindcss webpack test hello');
+
+const addTaskBtn = document.querySelector('.add-task');
+
+addTaskBtn.addEventListener('click', function () {
+	console.log('Click Click');
+});
+
+const newTask = new Task('bye', 'hello', 'hello');
+
+console.log(newTask);
