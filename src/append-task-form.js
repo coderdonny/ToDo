c165facb9423aa0215bb62ac22f6693f20d06867
Daniel Karpierz <dankarp1999@gmail.com>
2022-09-13 15:42:40 -0400

    Finish task form UI and create module for it

---
+++ src/append-task-form.js
@@ -0,0 +1,127 @@
+export function appendTaskForm() {
+	const addTaskBtn = document.querySelector('.add-task');
+	const taskList = document.querySelector('.task-list');
+
+	taskList.removeChild(addTaskBtn);
+	// addTaskBtn.removeEventListener('click', appendTaskForm);
+	const newLi = document.createElement('li');
+
+	const titleLabel = document.createElement('label');
+	const titleInput = document.createElement('input');
+
+	const detailsLabel = document.createElement('label');
+	const detailsInput = document.createElement('input');
+
+	const dateLabel = document.createElement('label');
+	const dateInput = document.createElement('input');
+
+	const deleteBtn = document.createElement('img');
+
+	const taskForm = document.createElement('form');
+
+	const submitBtn = document.createElement('button');
+
+	deleteBtn.src = './images/delete-icon.svg';
+
+	const row1 = document.createElement('p');
+	const row2 = document.createElement('p');
+	const row3 = document.createElement('p');
+
+	titleInput.setAttribute('type', 'text');
+	titleInput.setAttribute('id', 'title');
+	titleInput.setAttribute('placeholder', 'e.g., "Clean the dishes"');
+
+	titleLabel.setAttribute('for', 'title');
+	titleLabel.textContent = 'Title:';
+
+	detailsInput.setAttribute('type', 'text');
+	detailsInput.setAttribute('id', 'details');
+	detailsInput.setAttribute(
+		'placeholder',
+		'e.g., "Finish this task before Dad gets home from the trailer"'
+	);
+
+	detailsLabel.setAttribute('for', 'details');
+	detailsLabel.textContent = 'Details:';
+
+	dateInput.setAttribute('type', 'date');
+	dateInput.setAttribute('id', 'date');
+
+	dateLabel.setAttribute('for', 'date');
+	dateLabel.textContent = 'Due date:';
+
+	submitBtn.textContent = 'Add Task';
+
+	submitBtn.classList.add(
+		'hover:bg-buttonBlue',
+		'bg-highlightBlue',
+		'text-white',
+		'font-raj',
+		'font-200',
+		'py-1',
+		'px-10',
+		'rounded',
+		'mb-2'
+	);
+
+	titleInput.classList.add(
+		'bg-transparent',
+		'w-full',
+		'focus:border-none',
+		'focus:outline-none',
+		'border-transparent',
+		'focus:border-transparent',
+		'focus:ring-0'
+	);
+	detailsInput.classList.add(
+		'bg-transparent',
+		'w-full',
+		'focus:border-none',
+		'focus:outline-none',
+		'border-transparent',
+		'focus:border-transparent',
+		'focus:ring-0'
+	);
+
+	dateInput.classList.add('bg-transparent', 'ml-5', 'mb-2');
+
+	row1.classList.add('w-full', 'mb-2');
+	row2.classList.add('w-full', 'mb-2');
+	row3.classList.add('w-full');
+
+	taskForm.classList.add('flex-auto');
+	deleteBtn.classList.add(
+		'align-top',
+		'self-start',
+		'mr-2',
+		'mt-1',
+		'cursor-pointer'
+	);
+
+	newLi.classList.add(
+		'pl-4',
+		'border',
+		'rounded',
+		'border-lightBlue',
+		'align-middle',
+		'py-2',
+		'gap-2',
+		'flex'
+	);
+
+	row1.appendChild(titleLabel);
+	row1.appendChild(titleInput);
+	row2.appendChild(detailsLabel);
+	row2.appendChild(detailsInput);
+	row3.appendChild(dateLabel);
+	row3.appendChild(dateInput);
+
+	taskForm.appendChild(row1);
+	taskForm.appendChild(row2);
+	taskForm.appendChild(row3);
+	taskForm.appendChild(submitBtn);
+
+	newLi.appendChild(taskForm);
+	newLi.appendChild(deleteBtn);
+	taskList.appendChild(newLi);
+}
--- src/index.js
+++ src/index.js
@@ -1,73 +1,7 @@
 import './style.css';
 import Task from './create-task';
+import { appendTaskForm } from './append-task-form';
 
 const addTaskBtn = document.querySelector('.add-task');
-const taskList = document.querySelector('.task-list');
 
 addTaskBtn.addEventListener('click', appendTaskForm);
-
-function appendTaskForm() {
-	addTaskBtn.removeEventListener('click', appendTaskForm);
-	const newLi = document.createElement('li');
-	const titleLabel = document.createElement('label');
-	const titleInput = document.createElement('input');
-	const detailsLabel = document.createElement('label');
-	const detailsInput = document.createElement('input');
-	const row1 = document.createElement('p');
-	const row2 = document.createElement('p');
-
-	titleInput.setAttribute('type', 'text');
-	titleInput.setAttribute('id', 'title');
-
-	titleLabel.setAttribute('for', 'title');
-	titleLabel.textContent = 'Title:';
-
-	detailsInput.setAttribute('type', 'text');
-	detailsInput.setAttribute('id', 'details');
-
-	detailsLabel.setAttribute('for', 'details');
-	detailsLabel.textContent = 'Details:';
-
-	titleInput.classList.add('bg-transparent', 'ml-5');
-	detailsInput.classList.add('bg-transparent', 'ml-5');
-
-	row1.classList.add('flex');
-	row2.classList.add('flex');
-
-	newLi.classList.add(
-		'pl-4',
-		'flex-col',
-		'border',
-		'rounded',
-		'border-lightBlue',
-		'align-middle',
-		'py-2',
-		'gap-2'
-	);
-
-	addTaskBtn.classList.add(
-		'pl-4',
-		'flex-col',
-		'border',
-		'rounded',
-		'border-lightBlue',
-		'align-middle',
-		'py-2',
-		'gap-2'
-	);
-
-	row1.appendChild(titleLabel);
-	row1.appendChild(titleInput);
-	row2.appendChild(detailsLabel);
-	row2.appendChild(detailsInput);
-
-	newLi.appendChild(row1);
-	newLi.appendChild(row2);
-
-	taskList.appendChild(newLi);
-}
-
-// function addTask() {
-// 	const newTask = new Task('bye', 'hello', 'hii', 'hello');
-// 	console.log(newTask);
-// }
